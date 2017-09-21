27b18161c8a95bd22e0be42527584d3eca9bba50
John Nguyen <johnxuannguyen@gmail.com>
2017-09-21 12:48:06 +0200

    add chat heads vc to notification window

---
+++ Wire-iOS/Sources/UserInterface/Overlay/InAppNotifications/ChatHeadsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChatHeadsViewController.swift
+//  Wire-iOS
+//
+//  Created by John Nguyen on 21.09.17.
+//  Copyright © 2017 Zeta Project Germany GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ChatHeadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
